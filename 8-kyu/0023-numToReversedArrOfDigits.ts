ce88c5756e183ed09704725a056fcf686b1995c9
pierledev <pierledev@gmail.com>
2024-01-20 14:08:27 +0700

    Add solution for challenge 'Number to Reversed Array of Digits'

---
+++ 8-kyu/0023-numToReversedArrOfDigits.ts
@@ -0,0 +1,13 @@
+/*
+Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+Example(Input => Output):
+35231 => [1,3,2,5,3]
+0 => [0]
+*/
+
+const digitize = (n: number): number[] =>
+  `${n}`
+    .split('')
+    .map((digit) => +digit)
+    .reverse();
