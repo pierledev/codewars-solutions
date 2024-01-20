f6ee2723229d7decc308f5de4fbf3ff1e7a9372b
pierledev <pierledev@gmail.com>
2024-01-20 08:56:36 +0700

    Add solution for 'Multiply'

---
+++ 8-kyu/0004-multiply.ts
@@ -0,0 +1,9 @@
+/*
+This code does not execute properly. Try to figure out why.
+
+export function multiply(a: number, b: number): number {
+  return a + b;
+}
+*/
+
+const multiply = (a: number, b: number): number => a * b;
