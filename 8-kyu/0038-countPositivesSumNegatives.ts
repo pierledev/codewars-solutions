84ab4b0fa90dd5c3f22cba5b9de9a320469d0b0b
pierledev <pierledev@gmail.com>
2024-01-20 16:23:45 +0700

    Add solution for challenge 'Count Positives Sum negatives'

---
+++ 8-kyu/0038-countPositivesSumNegatives.ts
@@ -0,0 +1,24 @@
+/*
+Given an array of integers.
+
+Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+If the input is an empty array or is null, return an empty array.
+
+Example
+For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+*/
+
+const countPositivesSumNegatives = (input: any) => {
+  if (!input || input.length === 0) return [];
+
+  let countPositives = 0;
+  let sumNegatives = 0;
+
+  input.forEach((num: number) =>
+    num > 0 ? (countPositives += 1) : (sumNegatives += num)
+  );
+
+  return [countPositives, sumNegatives];
+};
+
