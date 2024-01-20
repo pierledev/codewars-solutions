6beccedc3ab607bca1e166e838db49bc6a94bc15
pierledev <pierledev@gmail.com>
2024-01-20 15:59:52 +0700

    Add solution for challenge 'Simple Multiplication'

---
+++ 8-kyu/0036-simpleMultiplication.ts
@@ -0,0 +1,6 @@
+/*
+This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+*/
+
+const simpleMultiplication = (num: number): number =>
+  num % 2 === 0 ? num * 8 : num * 9;
