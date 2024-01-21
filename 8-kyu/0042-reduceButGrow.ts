227f8143cd96f00e66453b87b3663b961c597b52
pierledev <pierledev@gmail.com>
2024-01-21 13:29:07 +0700

    Add solution for challenge 'Reduce but Grow'

---
+++ 8-kyu/0042-reduceButGrow.ts
@@ -0,0 +1,17 @@
+/*
+Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+[1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+*/
+
+// Solution 1
+const grow = (arr: number[]): number =>
+  arr.reduce((acc, curr) => acc * curr, 1);
+
+const grow2 = (arr: number[]): number => {
+  let result = 1;
+
+  for (const num of arr) result *= num;
+
+  return result;
+};
